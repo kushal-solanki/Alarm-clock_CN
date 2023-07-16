96128a638b86df0fdc30334527d640d005ec2ea9
kushal-solanki <114848773+kushal-solanki@users.noreply.github.com>
2023-07-16 17:42:20 +0530

    Add files via upload

---
+++ alarm.js
@@ -0,0 +1,144 @@
+const currentTime = document.querySelector("#current-time");
+const setHours = document.querySelector("#hours");
+const setMinutes = document.querySelector("#minutes");
+const setSeconds = document.querySelector("#seconds");
+const setAmPm = document.querySelector("#am-pm");
+const setAlarmButton = document.querySelector("#submitButton");
+const alarmContainer = document.querySelector("#alarms-container");
+
+// Adding Hours, Minutes, Seconds in DropDown Menu
+window.addEventListener("DOMContentLoaded", (event) => {
+  
+  dropDownMenu(1, 12, setHours);
+  dropDownMenu(0, 59, setMinutes);
+  dropDownMenu(0, 59, setSeconds);
+
+  setInterval(getCurrentTime, 1000);
+  fetchAlarm();
+});
+
+// Event Listener added to Set Alarm Button
+setAlarmButton.addEventListener("click", getInput);
+
+// Function to populate a dropdown menu with options
+function dropDownMenu(start, end, element) {
+  for (let i = start; i <= end; i++) {
+    const dropDown = document.createElement("option");
+    dropDown.value = i < 10 ? "0" + i : i;
+    dropDown.innerHTML = i < 10 ? "0" + i : i;
+    element.appendChild(dropDown);
+  }
+}
+
+// Function to get and display the current time
+function getCurrentTime() {
+  let time = new Date();
+  time = time.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  });
+  currentTime.innerHTML = time;
+
+  return time;
+}
+
+// Function to handle the click event of the Set Alarm button
+function getInput(e) {
+  e.preventDefault();
+  const hourValue = setHours.value;
+  const minuteValue = setMinutes.value;
+  const secondValue = setSeconds.value;
+  const amPmValue = setAmPm.value;
+
+  const alarmTime = convertToTime(
+    hourValue,
+    minuteValue,
+    secondValue,
+    amPmValue
+  );
+  setAlarm(alarmTime);
+}
+
+// Function to convert the selected time to a specific format
+function convertToTime(hour, minute, second, amPm) {
+  return `${parseInt(hour)}:${minute}:${second} ${amPm}`;
+}
+
+// Function to set an alarm and start the interval checking
+function setAlarm(time, fetching = false) {
+  const alarm = setInterval(() => {
+    if (time === getCurrentTime()) {
+      alert("Alarm Ringing");
+    }
+    console.log("running");
+  }, 500);
+
+  addAlaramToDom(time, alarm);
+  if (!fetching) {
+    saveAlarm(time);
+  }
+}
+
+// Function to add the alarm to the DOM
+function addAlaramToDom(time, intervalId) {
+  const alarm = document.createElement("div");
+  alarm.classList.add("alarm", "mb", "d-flex");
+  alarm.innerHTML = `
+    <div class="time">${time}</div>
+    <button class="btn delete-alarm" data-id=${intervalId}>Delete</button>
+  `;
+  const deleteButton = alarm.querySelector(".delete-alarm");
+  deleteButton.addEventListener("click", (e) => deleteAlarm(e, time, intervalId));
+
+  alarmContainer.prepend(alarm);
+}
+
+// Function to check if alarms are present in localStorage
+function checkAlarams() {
+  let alarms = [];
+  const isPresent = localStorage.getItem("alarms");
+  if (isPresent) alarms = JSON.parse(isPresent);
+
+  return alarms;
+}
+
+// Function to save the alarm to localStorage
+function saveAlarm(time) {
+  const alarms = checkAlarams();
+
+  alarms.push(time);
+  localStorage.setItem("alarms", JSON.stringify(alarms));
+}
+
+// Function to fetch alarms from localStorage and set them
+function fetchAlarm() {
+  const alarms = checkAlarams();
+
+  alarms.forEach((time) => {
+    setAlarm(time, true);
+  });
+}
+
+// Function to delete an alarm
+function deleteAlarm(event, time, intervalId) {
+  const self = event.target;
+
+  clearInterval(intervalId);
+
+  const alarm = self.parentElement;
+  console.log(time);
+
+  deleteAlarmFromLocal(time);
+  alarm.remove();
+}
+
+// Function to delete an alarm from localStorage
+function deleteAlarmFromLocal(time) {
+  const alarms = checkAlarams();
+
+  const index = alarms.indexOf(time);
+  alarms.splice(index, 1);
+  localStorage.setItem("alarms", JSON.stringify(alarms));
+}
